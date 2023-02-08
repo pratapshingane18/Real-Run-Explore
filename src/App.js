fc062983fd921813179f6afa4ce437a913294705
pratapshingane18 <sushrutrao465@gmail.com>
2023-02-09 00:17:51 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { CssBaseline, Grid} from '@material-ui/core';
+
+import Header from './components/Headers/Header';
+import Map from './Components/Map/Map';
+import List from './Components/List/List';
 
 const App = () =>{
     return (
-        <div>
-            <h1> Hello Prat's </h1>
-        </div>
+       <>
+       
+       <CssBaseline />
+       <Header />
+       <Grid container spacing ={3}  style={{width:'100%'}}>
+        <Grid item xs={12} ms={4}>
+        <List />
+        </Grid>
+        <Grid item xs={12} ms={8}>
+            <Map />
+
+         </Grid>
+            
+       </Grid>
+       </>
     );
 }
 
